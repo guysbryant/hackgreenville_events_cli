bba4e7444f973bcb0f2a44b6cb3b76a3129fadf9
Guy Bryant <guy.s.bryant@gmail.com>
2018-12-19 15:24:26 -0500

    first commit

---
+++ lib/hackgreenville_events_cli.rb
@@ -0,0 +1,6 @@
+require "hackgreenville_events_cli/version"
+
+module HackgreenvilleEventsCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hackgreenville_events_cli/version.rb
@@ -0,0 +1,3 @@
+module HackgreenvilleEventsCli
+  VERSION = "0.1.0"
+end
