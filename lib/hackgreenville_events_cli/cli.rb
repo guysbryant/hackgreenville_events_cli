ab07acb5818f793f17fabd016b34e254f3640113
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-02 15:33:32 -0500

    add case statement, not working

--- lib/hackgreenville_events_cli/cli.rb
+++ lib/hackgreenville_events_cli/cli.rb
@@ -16,6 +16,7 @@ class HackgreenvilleEventsCli::CLI
             how_many
        end
     end
+
     def list_events 
         @events = HackgreenvilleEventsCli::Events.all
         puts "\nHere are the upcoming events in Greenville, SC: \n\n"
@@ -27,42 +28,69 @@ class HackgreenvilleEventsCli::CLI
     def menu
         puts "Select an event by typing the associated number to get more information about that event or type exit to quit: \n\n"
         input = gets.strip
-        if input.class == String
-            if input.downcase == "exit"
-                quit 
-            end
-        end
-
         index = input.to_i - 1
-        wrong_input = "\n\nI don't understand\n\n".bold
-        if index >= 0 && index < @events.size
-            selected_event = @events[index]
+            case input
+            when "exit"
+                quit 
+            when "list"
+                list_events 
+                menu 
+            elsif input.to_i >= 0 && input.to_i <= @events.size 
+                    more_info(@events[index])
+                    menu
+                else
+                    puts "\n\nI don't understand\n\n".bold 
+                    menu 
+                end
+            end
+    end
+
+        # if input.class == String
+        #     if input.downcase == "exit"
+        #         quit 
+        #     end
+        # end
+
+        # index = input.to_i - 1
+        # wrong_input = 
+
+        def more_info(choice) 
+         selected_event = choice
             puts "#{selected_event.name}".green.bold.underline
             if !selected_event.description 
                 more_info = HackgreenvilleEventsCli::Scraper.new("#{selected_event.rsvp_url}").scrape_more_info
                 selected_event.add_info(more_info)
             end
             puts "  Time: ".red + "#{selected_event.time}", "  RSVP: ".red + "#{selected_event.rsvp_url}", "  Description: ".red + "#{selected_event.description}", "  Number of attendees: ".red + "#{selected_event.attendees}", "  Hosted by: ".red + "#{selected_event.hosted_by}", "  How often we meet: ".red + "#{selected_event.how_often}", "  Location: ".red + "#{selected_event.location}", "  How to find us: ".red + "#{selected_event.how_to_find_us}\n\n" 
-
-            while true 
-                puts "Type list to display the original list again, or exit to quit\n" 
-                input = gets.strip
-                if input.class == String
-                    if input.downcase == "list"
-                        list_events
-                        menu
-                    elsif input.downcase == "exit"
-                        quit
-                    else
-                        puts "#{wrong_input}"
-                    end
-                end
-            end
-        else 
-            puts "#{wrong_input}"
-            menu
         end
-    end
+        # if index >= 0 && index < @events.size
+            # selected_event = @events[index]
+            # puts "#{selected_event.name}".green.bold.underline
+            # if !selected_event.description 
+            #     more_info = HackgreenvilleEventsCli::Scraper.new("#{selected_event.rsvp_url}").scrape_more_info
+            #     selected_event.add_info(more_info)
+            # end
+            # puts "  Time: ".red + "#{selected_event.time}", "  RSVP: ".red + "#{selected_event.rsvp_url}", "  Description: ".red + "#{selected_event.description}", "  Number of attendees: ".red + "#{selected_event.attendees}", "  Hosted by: ".red + "#{selected_event.hosted_by}", "  How often we meet: ".red + "#{selected_event.how_often}", "  Location: ".red + "#{selected_event.location}", "  How to find us: ".red + "#{selected_event.how_to_find_us}\n\n" 
+
+        #     while true 
+        #         puts "Type list to display the original list again, or exit to quit\n" 
+        #         input = gets.strip
+        #         if input.class == String
+        #             if input.downcase == "list"
+        #                 list_events
+        #                 menu
+        #             elsif input.downcase == "exit"
+        #                 quit
+        #             else
+        #                 puts "#{wrong_input}"
+        #             end
+        #         end
+        #     end
+        # else 
+        #     puts "#{wrong_input}"
+        #     menu
+        # end
+    # end
 
     def quit 
         exit!
